7f414f2d413f01f0e17e6c944a088c5cce1e943d
zhuravskuyroma-afk <zhuravskuyroma@gmail.com>
2026-06-27 20:17:31 +0300

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,23 @@
 package com.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import com.example.AnchorAutomator;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(ClientPlayerInteractionManager.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+    
+    @Inject(method = "interactBlock", at = @At("TAIL"))
+    private void onInteractBlock(MinecraftClient client, Hand hand, BlockHitResult hitResult, CallbackInfoReturnable<ActionResult> cir) {
+        if (cir.getReturnValue() == ActionResult.SUCCESS || cir.getReturnValue() == ActionResult.CONSUME) {
+            AnchorAutomator.onBlockPlaced(hitResult);
+        }
+    }
 }
